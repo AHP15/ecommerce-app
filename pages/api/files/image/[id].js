ea3baca8f9d29b52673eafaf314970c936e30750
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-08-27 12:29:23 +0100

    add cors options to images api

--- pages/api/files/image/[id].js
+++ pages/api/files/image/[id].js
@@ -1,12 +1,30 @@
 
 import connectDB from "../../../../db/connection";
 import {GridFSBucket, ObjectId} from "mongodb";
+import Cors from 'cors'
+
+
+const cors = Cors({
+  methods: ['GET'],
+  origin:"*",
+});
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+      fn(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+  
+        return resolve(result)
+      })
+    })
+  }
 
 export default async function handler(req, res){
     const {id} = req.query;
 
     try{
-
+        await runMiddleware(req, res, cors);
         const DB = await connectDB();
         const bucket = new GridFSBucket(DB, { bucketName: 'productImages' });
         bucket.openDownloadStream(ObjectId(id)).pipe(res);
